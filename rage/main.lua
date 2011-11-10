59e4d62cefa2c6717821d8db15f83856cee28fa8
Bob Somers <bob@bobsomers.com>
2011-11-09 19:24:12 -0800

    Refactoring the player to use HUMPs class system.

--- rage/main.lua
+++ rage/main.lua
@@ -1,37 +1,23 @@
--- Renaming some libraries for convenience.
-local graphics = love.graphics
-local mouse = love.mouse
-local vector = require "hump.vector"
-
--- Some "constants" for the size of the screen.
+-- Some global "constants" for the size of the screen.
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
--- How much should we increase the players velocity? (in pixels per second)
-PLAYER_ACCEL = 300
-
--- How much velocity should we retain, every update? Think of this as a
--- percentage of the last update's velocity.
-PLAYER_FRICTION = 0.92
-
--- The speed of the level 1 enemies, in pixels per second.
-LEVEL1_SPEED = 100
+-- Load our Player class.
+love.filesystem.load("player.lua")()
 
 function love.load()
     -- Set the background color for when we redraw the frame.
-    graphics.setBackgroundColor(255, 255, 255)
+    love.graphics.setBackgroundColor(255, 255, 255)
 
     -- Seed the random number generator for a new experience every time!
     math.randomseed(os.time())
 
-    -- Create a table to hold information about the player.
-    player = {
-        image = graphics.newImage("assets/determined.png"),
-        position = vector(SCREEN_WIDTH * 3 / 4, SCREEN_HEIGHT * 3 / 4),
-        velocity = vector(0, 0)
-    }
+    -- Create a new player that accelerates at 300 pixels per second with 0.92
+    -- friction.
+    player = Player(300, 0.92)
 
     -- Create some tables to hold information about the enemies.
+    --[[
     level_images = {
         graphics.newImage("assets/happy.png")
     }
@@ -40,25 +26,14 @@ function love.load()
         position = vector(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4),
         velocity = vector((math.random() * 2) - 1, (math.random() * 2) - 1):normalize_inplace() * LEVEL1_SPEED
     }
+    --]]
 end
 
 function love.update(dt)
-    if mouse.isDown("l") then
-        -- If the player has their left mouse button down, add some velocity
-        -- to the player in the direction from the player to the mouse.
-        local player2mouse = vector(mouse.getX(), mouse.getY()) - player.position
-        player2mouse:normalize_inplace()
-        player.velocity = player.velocity + (player2mouse * PLAYER_ACCEL * dt)
-    else
-        -- If the player *doesn't* have their left mouse button down, bleed off
-        -- velocity to simulate friction.
-        player.velocity = player.velocity * PLAYER_FRICTION;
-    end
+    -- Update the player.
+    player:update(dt)
 
-    -- Update the player's position based on their current position and
-    -- velocity.
-    player.position = player.position + (player.velocity * dt)
-
+    --[[
     -- Update the enemies' positions.
     for i, enemy in ipairs(enemies) do
         enemy.position = enemy.position + (enemy.velocity * dt)
@@ -81,31 +56,20 @@ function love.update(dt)
             enemy.velocity.y = enemy.velocity.y * -1
         end
     end
+    --]]
 end
 
 function love.draw()
+    --[[
     -- Draw the enemies.
     for i, v in ipairs(enemies) do
         graphics.draw(level_images[1], v.position.x, v.position.y, 0, 1, 1,
             level_images[1]:getWidth() / 2, level_images[1]:getHeight() / 2)
     end
+    --]]
 
     -- Draw the player.
-    local player_direction = 1
-    if mouse.getX() < player.position.x then
-        player_direction = -1
-    end
-    graphics.draw(player.image, player.position.x, player.position.y, 0,
-        0.3 * player_direction, 0.3,
-        player.image:getWidth() / 2, player.image:getHeight() / 2)
-end
-
-function love.mousepressed(x, y, button)
-    if button == "l" then
-        local player2mouse = vector(x, y) - player.position
-        player2mouse:normalize_inplace()
-    end
-
+    player:draw()
 end
 
 function love.keypressed(key)
---
+++ rage/player.lua
@@ -0,0 +1,60 @@
+-- Let's use the class mechanism from HUMP.
+local Class = require "hump.class"
+
+-- And the vector helper!
+local Vector = require "hump.vector"
+
+-- Define our player's constructor.
+Player = Class(function(self, accel, friction)
+    -- How much should we increase the player's velocity? (in pixels per second)
+    self.accel = accel
+
+    -- How much velocity should we retain, every update? Think of this as a
+    -- percentage of the last update's velocity.
+    self.friction = friction
+
+    -- Load the image that will represent the player.
+    self.image = love.graphics.newImage("assets/determined.png")
+
+    -- Where should the player spawn?
+    self.position = Vector(SCREEN_WIDTH * 3 / 4, SCREEN_HEIGHT * 3 / 4)
+
+    -- What should the player's initial velocity be?
+    self.velocity = Vector(0, 0)
+end)
+
+function Player:update(dt)
+    if love.mouse.isDown("l") then
+        -- The player has their left mouse button down, so let's add some
+        -- velocity to the player in the direction from the player to the
+        -- mouse pointer.
+        local player2mouse = Vector(love.mouse.getX(), love.mouse.getY()) - self.position
+        player2mouse:normalize_inplace()
+        self.velocity = self.velocity + (player2mouse * self.accel * dt)
+    else
+        -- The player doesn't have their left mouse button down, so let's bleed
+        -- off some velocity to simulate friction.
+        self.velocity = self.velocity * self.friction
+    end
+
+    -- Update the player's position based on their current position and their
+    -- velocity.
+    self.position = self.position + (self.velocity * dt)
+end
+
+function Player:draw()
+    -- We'll "scale" our image on the X axis by either +1 or -1, depending on
+    -- which way he needs to be facing to look at the mouse.
+    local direction = 1
+    if love.mouse.getX() < self.position.x then
+        direction = -1
+    end
+
+    -- Draw our player at position (x, y), with no rotation, proper scaling
+    -- on the X axis (according to the direction above), 1.0 scale on the Y
+    -- axis, and defining the "center" of our player to be in the center of
+    -- the image.
+    love.graphics.draw(self.image, self.position.x, self.position.y, 0,
+        direction, 1, self.image:getWidth() / 2, self.image:getHeight() / 2)
+
+end
